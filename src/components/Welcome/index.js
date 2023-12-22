5b79b4594986a9b4142075265f5910883d8be268
chandramouli35 <tarigopulachandramouli1818@gmail.com>
2023-12-22 02:53:09 +0000

    Solution

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,29 @@
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  onSubscribe = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const buttonText = this.getButtonText()
+    return (
+      <div className="container">
+        <h1 className="heading">Welcome</h1>
+        <p className="paragraph">Thank You! Happy Learning </p>
+        <button type="button" onClick={this.onSubscribe} className="button">
+          {buttonText}
+        </button>
+      </div>
+    )
+  }
+}
+export default Welcome
